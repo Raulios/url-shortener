19c9a9cf63ce2c1fc81f05a3e99623d199037879
Raul Molina <rmolina@alintia.com>
2022-06-21 09:53:58 +0200

    refactor styles for smaller components
    refactor styles for smaller components

--- url-shortener/urlshortener/frontend/src/components/ItemList/component.js
+++ url-shortener/urlshortener/frontend/src/components/ItemList/component.js
@@ -3,7 +3,7 @@ import { Item } from "../ItemList/components/Item/component"
 
 const ItemList = (props) => {
 	return(
-		<div className="main-page__list">
+		<div className="item-list">
         {props.urlList.map((url) => (
           <Item key={url.short_url} url={url} handleDelete={props.handleDelete} />
         ))
--- url-shortener/urlshortener/frontend/src/components/ItemList/components/Item/component.js
+++ url-shortener/urlshortener/frontend/src/components/ItemList/components/Item/component.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 const Item = (props) => {
 	return(
-		<div className="main-page__list-item">
-	        <div className="main-page__text-wrapper">
-	          <span className="main-page__text main-page__text--highlighted">{props.url.short_url}</span>
-	          <span className="main-page__text">Redirects to: {props.url.original_url}</span>
+		<div className="item">
+	        <div className="item__text-wrapper">
+	          <span className="item__text item__text--highlighted">{props.url.short_url}</span>
+	          <span className="item__text">Redirects to: {props.url.original_url}</span>
 	        </div>
 	        <div>
-	          <Link className="main-page__link" to={`/stats/${props.url.id}`}>See Link Stats</Link>
+	          <Link className="item__link" to={`/stats/${props.url.id}`}>See Link Stats</Link>
 	          <button 
 	            type="submit"
-	            className="main-page__btn"
+	            className="item__btn"
 	            onClick={(e) => {props.handleDelete(e, props.url.id)}}
 	          >
 	            Delete
